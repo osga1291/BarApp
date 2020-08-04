648cbb26b18ea69bdf03b1b92f66290c1865663c
rj_fleur <rjfll24@gmail.com>
2020-08-04 17:11:15 -0600

    Updated users,drinks, and bars for CRUD

--- backend/routes/drinks.js
+++ backend/routes/drinks.js
@@ -14,8 +14,36 @@ router.route('/add').post((req,res)=> { //if post request with /add
     const newUser = new User({username}); // new user object instance  with this username
     
     newUser.save() //saved to db
-        .then(() => res.json('User added!'))  //return in json
+        .then(() => res.json('Drink added!'))  //return in json, change to dinks*
         .catch(err => res.status(400).json('Error:' + err));
+});
+
+router.route('/:id').get((req,res)=> { //if get request with route '/'
+    drinks.findById(req.params.id) // mongose gets all users in db
+    .then(drinks => res.json(drinks)) //return users in json format
+    .catch(err => res.status(400).json('Error:' + err)); //else error
+});
+
+router.route('/:id').delete((req,res)=> { //if get request with route '/'
+    drinks.findByIdAndDelete(req.params.id) // mongose gets all users in db
+    .then(() => res.json("Drink Deleted")) //return users in json format
+    .catch(err => res.status(400).json('Error:' + err)); //else error
+});
+
+router.route('/update/:id').post((req,res)=> { //if get request with route '/'
+    drinks.findById(req.params.id) // mongose gets all users in db
+    .then(drinks =>{
+        drinks.username = req.body.username;
+        drinks.name = req.body.name;
+        drinks.price = Number(req.body.price);
+        drinks.description = req.body.description;
+        drinks.drink_count = Number(req.body.drink_count);
+
+        drinks.save()
+          .then(() => res.json('Drinks updated!'))
+          .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.json.status(400).json('Error: ' + err));
 });
 
 module.exports = router; //exporting the router
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,4 +18,32 @@ router.route('/add').post((req,res)=> { //if post request with /add
         .catch(err => res.status(400).json('Error:' + err));
 });
 
+router.route('/:id').get((req,res)=> { //if get request with route '/'
+    user.findById(req.params.id) // mongose gets all users in db
+    .then(user => res.json(user)) //return users in json format
+    .catch(err => res.status(400).json('Error:' + err)); //else error
+});
+
+router.route('/:id').delete((req,res)=> { //if get request with route '/'
+    user.findByIdAndDelete(req.params.id) // mongose gets all users in db
+    .then(() => res.json("User Deleted")) //return users in json format
+    .catch(err => res.status(400).json('Error:' + err)); //else error
+});
+
+router.route('/update/:id').post((req,res)=> { //if get request with route '/'
+    user.findById(req.params.id) // mongose gets all users in db
+    .then(user =>{
+        user.username = req.body.username;
+        user.name = req.body.name;
+        user.imageURL = req.body.imageURL;
+        //user.favorite_bars = req.body.favorite_bars;
+        //user.favorite_drinks = req.body.favorite_drinks;
+
+        user.save()
+          .then(() => res.json('User updated!'))
+          .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.json.status(400).json('Error: ' + err));
+});
+
 module.exports = router; //exporting the router
